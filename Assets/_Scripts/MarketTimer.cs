336c5e2f3f7ec91be01813105421511c326ad783
Vincent-207 <vhannah2007@outlook.com>
2025-01-26 15:00:33 -0600

    Improvments

--- Assets/_Scripts/MarketTimer.cs
+++ Assets/_Scripts/MarketTimer.cs
@@ -12,11 +12,15 @@ public class MarketTimer : MonoBehaviour
     public float currentTimerDuration;
     private TMP_Text timerTextObject;
     private string timerText;
+    public float warningScaleScalar, warningTweenTime;
+    public Color warningTextColor;
+    Vector3 warningScale;
     // Start is called before the first frame update
     void Start()
     {
         timerTextObject = transform.GetChild(0).GetComponent<TMP_Text>();
         currentTimerDuration = timerDuration;
+        warningScale = new Vector3(warningScaleScalar, warningScaleScalar, warningScaleScalar);
     }
 
     // Update is called once per frame
@@ -24,6 +28,14 @@ public class MarketTimer : MonoBehaviour
     {
         currentTimerDuration -= Time.deltaTime;
         timerTextObject.text = currentTimerDuration.ToString("#.##"); // returns ".5" when decimalVar == 0.5m
+        if(currentTimerDuration <= 10 && transform.localScale != warningScale)
+        {
+            if(!LeanTween.isTweening(this.gameObject))
+            {
+                LeanTween.scale(this.gameObject, warningScale, warningTweenTime).setEaseOutElastic();
+                timerTextObject.color = warningTextColor;
+            }
+        }
         if(currentTimerDuration <= 0)
         {
             Debug.LogWarning("TRADE PERIOD END");
